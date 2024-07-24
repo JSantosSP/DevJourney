fa5cd8549677f20646c8dd68d683fa389ddff9aa
JSantosSP <jsantospalomera@gmail.com>
2024-07-24 14:37:14 +0200

    testCVpdf

--- src/Components/Base/ButtonCV.js
+++ src/Components/Base/ButtonCV.js
@@ -3,7 +3,7 @@ import { FloatButton } from 'antd';
 const handleDownloadAndRedirect = () => {
     // Descargar el archivo PDF
     const link = document.createElement('a');
-    link.href = '/CV_jose_miguel_santos_palomera.pdf';
+    link.href = '/DevJourney/build/CV_jose_miguel_santos_palomera.pdf';
     link.download = 'CV_jose_miguel_santos_palomera.pdf';
     document.body.appendChild(link);
     link.click();
